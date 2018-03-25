291ec62459dd05d4cda1d2be8af5cc99590435ff
Shifa Samreen <shifasamreen24@gmail.com>
2018-03-26 00:58:55 +0530

    Finally, after a lot of debugging, removed the error of nodemon crashing AND was able to post a new feed. The issue was in the body parser. Please take a look at the commented section for the lines of code that I added to remove the undefined error on the terminal

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+//make allFeeds global!
+var allFeeds=[" Hello there! This is a simple attempt to begin developing in MEAN stack.",
+                " Not trying to create something big as of now.",
+                "Just want  to get started, so starting of small.",
+                "Are you fine, by the way?"]; 
 
 app.get('/myfeeds', function(req, res, next){
-   var allFeeds=[" Hello there! This is a simple attempt to begin developing in MEAN stack.",
-                     " Not trying to create something big as of now.",
-                    "Just want  to get started, so starting of small.",
-                   "Are you fine, by the way?"]; 
     
     return res.send(allFeeds);
 });
+
+app.post('/myfeeds', function(req, res, next){
+   //console.log(req.body);
+    allFeeds.push(req.body.newFeed);
+    res.send();
+}); 
 
 app.listen('3000', function(){
    console.log("Listening"); 
