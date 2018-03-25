155e4513d525692ba3e1417e1f2bb626fbb0e089
Shifa Samreen <shifasamreen24@gmail.com>
2018-03-26 01:02:31 +0530

    Finally removed the error of nodemon crashing, ny killall -9 node command process killing. After this resolved the undefined body parser error by adding three lines of code in the server.Look for the comments

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+//Added the below one lines to remove undefined body parser error.
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('public'));
+//Added the below two lines to remove undefined body parser error
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
