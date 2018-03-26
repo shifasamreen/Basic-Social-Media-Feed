282537e251d542689fc3105531eddfc9ae89cba4
Shifa Samreen <shifasamreen24@gmail.com>
2018-03-26 22:33:24 +0530

    Linked Mongodb and stored the data there, and accessed the data from the mongodb

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
 //Added the below one lines to remove undefined body parser error.
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
+
+var db=null;
+
+//connect to Mongo
+MongoClient.connect("mongodb://localhost:27017/feeds", function(err, dbconn){
+    if(!err){
+        console.log("We are connected");
+        db=dbconn;
+    }
+    
+});
 
 app.use(express.static('public'));
 //Added the below two lines to remove undefined body parser error
@@ -9,20 +21,24 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 //make allFeeds global!
-var allFeeds=[" Hello there! This is a simple attempt to begin developing in MEAN stack.",
-                " Not trying to create something big as of now.",
-                "Just want  to get started, so starting of small.",
-                "Are you fine, by the way?"]; 
 
 app.get('/myfeeds', function(req, res, next){
+    db.collection('feeds', function(err, feedsCollection){
+        feedsCollection.find().toArray(function(err, feeds){
+           return res.json(feeds); 
+        });
+    });
     
-    return res.send(allFeeds);
 });
 
 app.post('/myfeeds', function(req, res, next){
    //console.log(req.body);
-    allFeeds.push(req.body.newFeed);
-    res.send();
+    db.collection('feeds', function(err, feedsCollection){
+        feedsCollection.insert({text: req.body.newFeed},{w:1},function(err, feeds){
+           return res.send(); 
+        });
+    });
+    
 }); 
 
 app.listen('3000', function(){
